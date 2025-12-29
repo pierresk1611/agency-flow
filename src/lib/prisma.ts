3d0bc40f01bac73d78b8e4c9e345e1675481687f
pierresk1611 <peter.duris.zv@gmail.com>
2025-12-29 21:11:25 +0100

    fix(db): force persistent database connection

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,6 +4,13 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+console.log("DEBUG: process.env.DATABASE_URL =", process.env.DATABASE_URL);
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({
+  datasources: {
+    db: {
+      url: "postgresql://neondb_owner:npg_Aw56YZHlVUhO@ep-calm-violet-aggutujf-pooler.c-2.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+    },
+  },
+})
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
